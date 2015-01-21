237a03a2b274757dd4e8f2037147fa4d2ec00886
Michael Snow <michael.snow@rackspace.com>
2015-01-20 20:15:52 -0500

    More helpful bits

--- Vagrantfile
+++ Vagrantfile
@@ -40,7 +40,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
   # config.vm.synced_folder "../data", "/vagrant_data"
-  config.vm.synced_folder ".", "/var/www/vhosts/vt-one.org/htdocs"
+  config.vm.synced_folder "../website", "/var/www/vhosts/vt-one.org/htdocs/wp",
+    :owner => "apache", :group => "apache", :mount_options => ['dmode=775', 'fmode=775']
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
--- fix_site_name.sh
+++ fix_site_name.sh
@@ -2,21 +2,15 @@
 
 CONFIG=$1
 SITE=$2
-BACKUP=$3
 
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 CONFIG SITE BACKUP_FILE"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 CONFIG SITE"
 	exit 1
 fi
 
 if [ -z "$SITE" ]; then
 	echo "Please provide a site name"
 	exit 1
-fi
-
-if [ -z "$BACKUP" ]; then
-	echo "Please provide a backup file name"
-	exit 2
 fi
 
 if [ ! -r "$CONFIG" ]; then
@@ -31,17 +25,6 @@ DB_NAME=$(awk -F\' '/DB_NAME/ {print $4}' $CONFIG)
 DB_USER=$(awk -F\' '/DB_USER/ {print $4}' $CONFIG)
 DB_PASSWORD=$(awk -F\' '/DB_PASSWORD/ {print $4}' $CONFIG)
 DB_HOST=$(awk -F\' '/DB_HOST/ {print $4}' $CONFIG)
-
-echo "Reading database from $BACKUP into $DB_NAME"
-BACKUPSQL=$(basename $BACKUP .gz)
-gunzip -c $BACKUP > $BACKUPSQL
-mysql5 -u $DB_USER -p$DB_PASSWORD -h $DB_HOST $DB_NAME < $BACKUPSQL
-
-if [ $? -ne 0 ]; then
-	echo "Backup failed."
-	exit 1
-fi
-rm -f $BACKUPSQL
 
 echo "Correcting site URL"
 mysql -u $DB_USER -p$DB_PASSWORD -h $DB_HOST $DB_NAME -e "UPDATE wp_options SET option_value='http://$SITE/wp' WHERE option_name='siteurl';"
